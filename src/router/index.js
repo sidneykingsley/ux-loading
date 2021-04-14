f4a741fdcb0b5cc4e2960ef641df29894fbcb086
Sidney Kingsley <sidney.kingsley@students.plymouth.ac.uk>
2021-04-14 01:29:43 +0100

    changed to hash routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Experiment from '../views/Experiment.vue'
 import Results from '../views/Results.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 })
 
